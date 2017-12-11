7d21a67a115c27d11f2ebb538731c09649f832e7
Aleksey Vyaznikov <alexv47@mail.ru>
2017-12-11 17:21:30 +0300

    comm

--- TestTCPClientServerSolution/TCPClient/ClientTCP.cs
+++ TestTCPClientServerSolution/TCPClient/ClientTCP.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +10,68 @@ namespace TCPClient
 {
     internal class ClientTcp
     {
+        private Socket _socket;
+
+
+        #region Events
+
+        #endregion
+
+        #region Methods
+
+        public void Connect(IPAddress ipAddress, int portNum)
+        {
+            try
+            {
+                Disconnect();
+
+                _socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                var ePoint = new IPEndPoint(ipAddress, portNum);
+                _socket.Connect(ePoint);
+                if (_socket.Connected)
+                {
+                    _awaitRecieveData();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e); 
+            }
+        }
+
+        public void Disconnect()
+        {
+            _socket?.Close();
+        }
+
+        private void _awaitRecieveData()
+        {
+            try
+            {
+                var args = new SocketAsyncEventArgs();
+                args.Completed += _argsOnCompleted;
+                _socket.ReceiveAsync(args);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        private void _argsOnCompleted(object sender, SocketAsyncEventArgs socketAsyncEventArgs)
+        {
+            
+        }
+
+        #endregion
+
+        #region Properties
+
+        public bool IsConnected => _socket != null && _socket.Connected;
+
+        #endregion
+
 
     }
 }
--- TestTCPClientServerSolution/TCPClient/MainWindow.xaml.cs
+++ TestTCPClientServerSolution/TCPClient/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -22,27 +24,33 @@ namespace TCPClient
     {
         private ClientTcp _clientTcp;
         private bool _IsConnectValid;
+        //private bool _toogleConnect;
+
+        private string _serverName; //TODO get from Configuration
+        private string _portNum;      //TODO get from Configuration
 
         public MainWindow()
         {
             InitializeComponent();
             _refersh();
+            _serverName = "localhost";
+            _portNum = "11111";
         }
 
         #region Events
 
         private void BtnConnDisc_Click(object sender, RoutedEventArgs e)
         {
-            _IsConnectValid = _connectServer();
-            _refersh();
-            if (_IsConnectValid)
+            if (_IsConnectValid == false)
             {
-                
+                _IsConnectValid = _connectServer();
             }
             else
             {
-                
+                _disconnectServer();
+                _IsConnectValid = false;
             }
+            _refersh();
         }
 
         private void BtnStartStop_Click(object sender, RoutedEventArgs e)
@@ -76,10 +84,63 @@ namespace TCPClient
             }
             else
             {
-                return true;
+                if(_clientTcp.IsConnected)
+                    return true;
+            }
+
+            var ipAddr = _resolveIpAddress();
+            if (ipAddr == null) return false;
+
+            if (!int.TryParse(_portNum, out int port))
+            {
+                port = 11111;
+            }
+
+            try
+            {
+                _clientTcp.Connect(ipAddr, port);
+                return _clientTcp.IsConnected;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
             }
 
             return false;
+        }
+
+        private void _disconnectServer()
+        {
+            if(_clientTcp == null) return;
+
+            _clientTcp.Disconnect();
+        }
+
+        private IPAddress _resolveIpAddress()
+        {
+            var addrServer = _serverName;
+            if (string.IsNullOrEmpty(addrServer)) return null;
+
+            var addrSplit = addrServer.Split('.');
+            if (addrSplit.Length == 4) return IPAddress.Parse(addrServer);
+
+            try
+            {
+                var ipHostEntry = Dns.GetHostEntry(addrServer);
+                foreach (var ipAddr in ipHostEntry.AddressList)
+                {
+                    if (ipAddr.AddressFamily != AddressFamily.InterNetwork) continue;
+
+                    addrServer = ipAddr.ToString();
+                    break;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+
+            }
+            return IPAddress.Parse(addrServer);
         }
 
         #endregion
