755aa637d6adc807c89b4346f703f43ae0b8125a
Aleksey Vyaznikov <alexv47@mail.ru>
2017-12-11 12:42:40 +0300

    add console server

---
+++ TestTCPClientServerSolution/TCPServerHost/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCPServerHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int portNum = 11111;
+            var srv = new Server();
+            srv.Start(portNum);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TestTCPClientServerSolution/TCPServerHost/Server.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCPServerHost
+{
+    internal class Server
+    {
+        private Socket _listenerSocket;
+
+        public void Start(int listenPort)
+        {
+            try
+            {
+                //var host = Dns.GetHostEntry("127.0.0.1");
+                //var ipAddrList = host.AddressList;
+                //if (!ipAddrList.Any()) return;
+
+                //var ipAddr = ipAddrList[0];
+                var ePoint = new IPEndPoint(IPAddress.Any, listenPort);
+                _listenerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                _listenerSocket.Bind(ePoint);
+                _listenerSocket.Listen(10);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+        }
+    }
+}
