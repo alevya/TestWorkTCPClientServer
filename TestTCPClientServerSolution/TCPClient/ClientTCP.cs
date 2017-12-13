a2bb422c4191cd630f95bf58b7d3f08a1f53f93a
Aleksey Vyaznikov <alexv47@mail.ru>
2017-12-13 15:02:01 +0300

    commit

--- TestTCPClientServerSolution/TCPClient/ClientTCP.cs
+++ TestTCPClientServerSolution/TCPClient/ClientTCP.cs
@@ -1,18 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 using System.Net;
 using System.Net.Sockets;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TCPClient
 {
     internal class ClientTcp
     {
         private Socket _socket;
-
 
         #region Events
 
